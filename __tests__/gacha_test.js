c15ebd39b803577046f1e6c4fce748cebd0173a1
thr3a <a@example.com>
2020-09-21 14:07:22 +0900

    add: X％成功する回数求める関数実装

--- __tests__/gacha_test.js
+++ __tests__/gacha_test.js
@@ -1,22 +1,26 @@
-const Gacha = require('../lib/gacha');
+const Gacha = require('../lib/gacha')
 
 test('出現率1%を100回行った時1回以上成功する確率', () => {
-  const gacha = new Gacha(1, 100);
-  expect(gacha.anySuccessProb().format()).toBe('63.40%');
-});
+  const gacha = new Gacha(1, 100)
+  expect(gacha.anySuccessProb().format()).toBe('63.40%')
+})
 
 test('出現率1%を100回行った時すべて失敗する確率', () => {
-  const gacha = new Gacha(1, 100);
-  expect(gacha.allFailProb().format()).toBe('36.60%');
-});
+  const gacha = new Gacha(1, 100)
+  expect(gacha.allFailProb().format()).toBe('36.60%')
+})
 
 test('出現率1%を100回行った時2回成功する確率', () => {
-  const gacha = new Gacha(1, 100);
-  expect(gacha.SuccessProbByHit(2).format()).toBe('18.49%');
-});
+  const gacha = new Gacha(1, 100)
+  expect(gacha.SuccessProbByHit(2).format()).toBe('18.49%')
+})
 
 test('出現率1%を100回行った時2回以上成功する確率', () => {
-  const gacha = new Gacha(1, 100);
-  expect(gacha.SuccessProbByHits(2).format()).toBe('26.42%');
-});
+  const gacha = new Gacha(1, 100)
+  expect(gacha.SuccessProbByHits(2).format()).toBe('26.42%')
+})
 
+test('出現率1%を100回行った時2回以上成功する確率', () => {
+  const gacha = new Gacha(1, 100)
+  expect(gacha.anySuccessCount(50)).toBe(69)
+})
