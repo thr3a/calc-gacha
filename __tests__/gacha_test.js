8a01445703be328fbf44b6e0e850882240c06892
thr3a <a@example.com>
2020-09-21 13:45:05 +0900

    add: eslint導入

--- __tests__/gacha_test.js
+++ __tests__/gacha_test.js
@@ -1,21 +1,21 @@
 const Gacha = require('../lib/gacha');
 
-test('1%の確率を100回行った時1回以上成功する確率', () => {
+test('出現率1%を100回行った時1回以上成功する確率', () => {
   const gacha = new Gacha(1, 100);
   expect(gacha.anySuccessProb().format()).toBe('63.40%');
 });
 
-test('1%の確率を100回行った時すべて失敗する確率', () => {
+test('出現率1%を100回行った時すべて失敗する確率', () => {
   const gacha = new Gacha(1, 100);
   expect(gacha.allFailProb().format()).toBe('36.60%');
 });
 
-test('1%の確率を100回行った時2回成功する確率', () => {
+test('出現率1%を100回行った時2回成功する確率', () => {
   const gacha = new Gacha(1, 100);
   expect(gacha.SuccessProbByHit(2).format()).toBe('18.49%');
 });
 
-test('1%の確率を100回行った時2回以上成功する確率', () => {
+test('出現率1%を100回行った時2回以上成功する確率', () => {
   const gacha = new Gacha(1, 100);
   expect(gacha.SuccessProbByHits(2).format()).toBe('26.42%');
 });
--- sample.js
+++ sample.js
@@ -1,10 +1,10 @@
-const Gacha = require('./lib/gacha.js');  // コンストラクタが返される
+const Gacha = require('./lib/gacha.js')  // コンストラクタが返される
 
-const prob = 1;
-const number = 100;
-const gacha = new Gacha(prob, number);
+const prob = 1
+const number = 100
+const gacha = new Gacha(prob, number)
 
-console.log(gacha.anySuccessProb().format());
-console.log(gacha.allFailProb().format());
-console.log(gacha.SuccessProbByHit(2).format());
-console.log(gacha.SuccessProbByHits(2).format());
+console.log(gacha.anySuccessProb().format())
+console.log(gacha.allFailProb().format())
+console.log(gacha.SuccessProbByHit(2).format())
+console.log(gacha.SuccessProbByHits(2).format())
